4044da550872c2d7805c80877763dda386b04e58
Hernan Pepe Petersen <hppeter@ar.ibm.com>
2020-02-26 17:56:24 -0300

    Reordering repo

---
+++ src/test/java/com/upgrade/backendchallenge/TestUtil.java
@@ -0,0 +1,24 @@
+package com.upgrade.backendchallenge;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestUtil {
+
+	public static void runMultithreaded(Runnable runnable, int threadCount) throws InterruptedException {
+		List<Thread> threadList = new LinkedList<Thread>();
+
+		for (int i = 0; i < threadCount; i++) {
+			threadList.add(new Thread(runnable));
+		}
+
+		for (Thread t : threadList) {
+			t.start();
+		}
+
+		for (Thread t : threadList) {
+			t.join();
+		}
+	}
+
+}
